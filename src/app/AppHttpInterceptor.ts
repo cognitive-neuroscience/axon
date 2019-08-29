69cd2a961f12cf824824adf09bf26f3a4b3b8543
Praveen Ravichandran <praveen.ravichandran@mail.mcgill.ca>
2019-08-29 18:58:49 -0400

    Remove logs from interceptors

--- src/app/AppHttpInterceptor.ts
+++ src/app/AppHttpInterceptor.ts
@@ -49,12 +49,8 @@ export class AppHttpInterceptor implements HttpInterceptor {
         }
 
         return next.handle(request).pipe(tap((event: HttpEvent<any>) => {
-            if (event instanceof HttpResponse) {
-                console.log(`[${event.status}] ${event.statusText}\n${event.url.replace(/\?[\D\d\s]+/gi, "")}`);
-            }
         }, (err: any) => {
             if (err instanceof HttpErrorResponse) {
-                console.error(`[${err.status}] ${err.statusText}\n${err.url}\n${err.message}`);
                 if ((err.status === 401) || (err.status === 403)) {
                     this.router.navigate(["/public/login"]);
                     localStorage.removeItem('token');
