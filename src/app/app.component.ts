e05cbfa476f700f1e3c5c8760c93c396607b015b
Praveen Ravichandran <praveen.ravichandran@mail.mcgill.ca>
2019-08-29 22:39:41 -0400

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   template: `<router-outlet></router-outlet>`,
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'axon';
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.navigate(['/dashboard']);
+  }
+
 }
