5b43771f59fcbde360b3601b3ed211916e3c9373
Praveen Ravichandran <praveen.ravichandran@mail.mcgill.ca>
2019-08-31 12:39:04 -0400

    Updated experiment model

--- src/app/models/Experiment.ts
+++ src/app/models/Experiment.ts
@@ -4,4 +4,13 @@ export class Experiment {
     description: string = '';
     route: string = '';
     type: string = '';
+    isScored: boolean = true;
+    showFeedbackAfterEveryTrial: boolean = true;
+    showScoreAfterEveryTrial: boolean = true;
+    numberOfBreaks: number = 0;
+    maxResponseTime: number = 800;
+    durationOfFeedback: number = 1000;
+    interTrialDelay: number = 1000;
+    practiceTrials: number = 1;
+    actualTrials: number = 10;
 }
