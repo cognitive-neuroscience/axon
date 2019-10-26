629bb061b1c2cb9deeb92efe66af396bce890cb2
Praveen Ravichandran <praveen.ravichandran@mail.mcgill.ca>
2019-10-26 18:28:01 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseURL: "https://sharplab.praveenravichandran.xyz/api"
+  apiBaseURL: `${window.location.protocol}//api.${window.location.host}/api`
 };
