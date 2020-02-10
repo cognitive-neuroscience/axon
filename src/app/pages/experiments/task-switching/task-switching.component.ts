828f493a41cce33f8c5c9d225646d5b6295881d1
Praveen Ravichandran <praveen.ravichandran@mail.mcgill.ca>
2020-02-09 20:07:52 -0500

    Added task switching

--- src/app/pages/experiments/task-switching/task-switching.component.ts
+++ src/app/pages/experiments/task-switching/task-switching.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from 'src/app/services/data.service';
+import { Matrix } from './matrix';
+declare function setFullScreen(): any;
 @Component({
   selector: 'app-task-switching',
   templateUrl: './task-switching.component.html',
@@ -7,9 +10,451 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskSwitchingComponent implements OnInit {
 
-  constructor() { }
+  // Default Experiment config 
+  isScored: boolean | number = true;
+  showFeedbackAfterEveryTrial: boolean | number = true;
+  showScoreAfterEveryTrial: boolean | number = true;
+  numberOfBreaks: number = 2;
+  maxResponseTime: number = 800;        // In milliseconds
+  durationOfFeedback: number = 500;    // In milliseconds
+  interTrialDelay: number = 1000;       // In milliseconds
+  practiceTrials: number = 10;
+  actualTrials: number = 30;
 
+  step: number = 1;
+  color: string = 'transparent';
+  number: number = 0;
+  feedback: string = '';
+  scoreForSpecificTrial: number = 0;
+  totalScore: number = 0;
+  isPractice: boolean = false;
+  isStimulus: boolean = false;
+  isBreak: boolean = false;
+  currentTrial: number = 0;
+  isResponseAllowed: boolean = false;
+  data: {
+    color: string,
+    digit: number,
+    actualAnswer: string,
+    userAnswer: string,
+    responseTime: number,
+    isCorrect: number,
+    score: number
+  }[] = [];
+  timer: {
+    started: number,
+    ended: number
+  } = {
+      started: 0,
+      ended: 0
+    };
+  showFixation: boolean = false;
+  sTimeout: any;
+  feedbackShown: boolean = false;
+  matrix = new Matrix();
+
+  @HostListener('document:click', ['$event'])
+  onKeyPress(event: MouseEvent) {
+    if (this.isResponseAllowed) {
+      this.isResponseAllowed = false;
+      try {
+        if (this.data[this.data.length - 1].color === 'blue') {
+          this.timer.ended = new Date().getTime();
+          this.data[this.data.length - 1].responseTime = Number(((this.timer.ended - this.timer.started) / 1000).toFixed(2));
+          this.data[this.data.length - 1].userAnswer = 'LESSER';
+        } else {
+          this.timer.ended = new Date().getTime();
+          this.data[this.data.length - 1].responseTime = Number(((this.timer.ended - this.timer.started) / 1000).toFixed(2));
+          this.data[this.data.length - 1].userAnswer = 'ODD';
+        }
+        try {
+          clearTimeout(this.sTimeout);
+          this.showFeedback();
+        } catch (error) {
+        }
+      } catch (error) {
+      }
+    }
+  }
+
+
+  /**
+   * Creates an instance of GoNogoComponent
+   * 
+   * @param {Router} router
+   * @memberof GoNogoComponent
+   */
+  constructor(
+    private router: Router,
+    private dataService: DataService
+  ) { }
+
+
+  /**
+   * ngOnInit lifecycle hook
+   *
+   * @memberof GoNogoComponent
+   */
   ngOnInit() {
+    let route_split = this.router.url.split('/');
+    let routePath = route_split[route_split.length - 1];
+    let currentExperiment = this.dataService.getExperimentByRoute(routePath);
+    this.isScored = currentExperiment.isScored
+    this.showFeedbackAfterEveryTrial = currentExperiment.showFeedbackAfterEveryTrial
+    this.showScoreAfterEveryTrial = currentExperiment.showScoreAfterEveryTrial
+    this.numberOfBreaks = currentExperiment.numberOfBreaks
+    this.maxResponseTime = currentExperiment.maxResponseTime
+    this.durationOfFeedback = currentExperiment.durationOfFeedback
+    this.interTrialDelay = currentExperiment.interTrialDelay
+    this.practiceTrials = currentExperiment.practiceTrials
+    this.actualTrials = currentExperiment.actualTrials
+  }
+
+
+  /**
+   * Process consent
+   *
+   * @param {Boolean} consent
+   * @memberof GoNogoComponent
+   */
+  processConsent(consent: Boolean) {
+    if (consent) {
+      this.proceedtoNextStep();
+    } else {
+      this.router.navigate(['/dashboard']);
+    }
+  }
+
+
+  /**
+   * Go back to previous step
+   *
+   * @memberof GoNogoComponent
+   */
+  proceedtoPreviousStep() {
+    this.step -= 1;
+  }
+
+
+  /**
+   * Proceed to next step
+   *
+   * @memberof GoNogoComponent
+   */
+  proceedtoNextStep() {
+    this.step += 1;
+  }
+
+  processClickEvent(event: any) {
+    if (this.isResponseAllowed) {
+      this.isResponseAllowed = false;
+      try {
+        if (this.data[this.data.length - 1].color === 'blue') {
+          this.timer.ended = new Date().getTime();
+          this.data[this.data.length - 1].responseTime = Number(((this.timer.ended - this.timer.started) / 1000).toFixed(2));
+          this.data[this.data.length - 1].userAnswer = 'GREATER';
+        } else {
+          this.timer.ended = new Date().getTime();
+          this.data[this.data.length - 1].responseTime = Number(((this.timer.ended - this.timer.started) / 1000).toFixed(2));
+          this.data[this.data.length - 1].userAnswer = 'EVEN';
+        }
+        try {
+          clearTimeout(this.sTimeout);
+          this.showFeedback();
+        } catch (error) {
+        }
+      } catch (error) {
+      }
+    }
+    event.preventDefault();
+  }
+
+
+  /**
+   * Start the practice session
+   *
+   * @memberof GoNogoComponent
+   */
+  async startPractice() {
+    this.startGameInFullScreen();
+    this.resetData();
+    this.proceedtoNextStep();
+    await this.wait(2000);
+    this.proceedtoNextStep();
+    this.isPractice = true;
+    this.currentTrial = 0;
+    this.showStimulus();
+  }
+
+
+  /**
+   * Start actual game
+   *
+   * @memberof GoNogoComponent
+   */
+  async startActualGame() {
+    this.resetData();
+    this.proceedtoNextStep();
+    await this.wait(2000);
+    this.proceedtoNextStep();
+    this.isPractice = false;
+    this.currentTrial = 0;
+    this.showStimulus();
+  }
+
+
+  /**
+   * Show stimulus
+   *
+   * @memberof GoNogoComponent
+   */
+  async showStimulus() {
+
+    this.reset();
+    this.showFixation = true;
+    await this.wait(500);
+    this.showFixation = false;
+    await this.wait(200);
+
+    this.currentTrial += 1;
+    this.generateStimulus();
+    this.isStimulus = true;
+    this.isResponseAllowed = true;
+
+    this.timer.started = new Date().getTime();
+    this.timer.ended = 0;
+
+    console.log(this.isPractice ? `Practice trial: ${this.currentTrial}` : `Actual trial: ${this.currentTrial}`);
+
+    // This is the delay between showing the stimulus and showing the feedback
+    this.sTimeout = setTimeout(() => {
+      if (!this.feedbackShown) {
+        this.showFeedback();
+      }
+    }, this.maxResponseTime);
+  }
+
+
+  /**
+   * Generate target stimulus
+   *
+   * @memberof GoNogoComponent
+   */
+  generateStimulus() {
+    const rand = this.matrix.colors[this.currentTrial];
+
+    let color = 'blue';
+    if (rand === 1) {
+      color = 'blue';
+    } else {
+      color = 'green';
+    }
+
+    const digit = this.matrix.digits[this.currentTrial];
+
+    let answer = '';
+
+    if (color === 'green') {
+      if (digit % 2 === 0) {
+        answer = 'EVEN';
+      } else {
+        answer = 'ODD';
+      }
+    } else {
+      if (digit > 5) {
+        answer = 'GREATER';
+      } else {
+        answer = 'LESSER';
+      }
+    }
+
+    this.color = color;
+    this.number = digit;
+
+    this.data.push({
+      color: color,
+      digit: digit,
+      actualAnswer: answer,
+      userAnswer: '',
+      responseTime: 0,
+      isCorrect: 0,
+      score: 0
+    });
+
+  }
+
+
+  /**
+   * Show feedback
+   *
+   * @memberof GoNogoComponent
+   */
+  async showFeedback() {
+    this.feedbackShown = true;
+    this.isStimulus = false;
+    this.isResponseAllowed = false;
+
+    if (this.data[this.data.length - 1].responseTime === 0) {
+      this.timer.ended = new Date().getTime();
+      this.data[this.data.length - 1].responseTime = Number(((this.timer.ended - this.timer.started) / 1000).toFixed(2));
+    }
+
+    if (this.data[this.data.length - 1].actualAnswer === this.data[this.data.length - 1].userAnswer) {
+      this.feedback = "Correct";
+      this.data[this.data.length - 1].isCorrect = 1;
+      this.data[this.data.length - 1].score = 10;
+      this.scoreForSpecificTrial = 10;
+      this.totalScore += 10;
+    } else {
+      if (this.data[this.data.length - 1].userAnswer === '') {
+        this.feedback = "Too slow";
+      } else {
+        this.feedback = "Incorrect";
+      }
+      this.data[this.data.length - 1].isCorrect = 0;
+      this.data[this.data.length - 1].score = 0;
+      this.scoreForSpecificTrial = 0;
+    }
+
+    if (this.showFeedbackAfterEveryTrial || this.isPractice) {
+      await this.wait(this.durationOfFeedback);
+    }
+    this.decideToContinue();
+  }
+
+
+  /**
+   * Check if trials have been finished
+   *
+   * @memberof GoNogoComponent
+   */
+  async decideToContinue() {
+    if (this.isPractice) {
+      if (this.currentTrial < this.practiceTrials) {
+        this.continueGame();
+      } else {
+        this.proceedtoNextStep();
+        await this.wait(2000);
+        this.proceedtoNextStep();
+      }
+    } else {
+      if (this.currentTrial < this.actualTrials) {
+        if (this.numberOfBreaks === 0) {
+          this.continueGame();
+        } else {
+          let breakAtTrailIndices = [];
+          let setSize = this.actualTrials / (this.numberOfBreaks + 1);
+          for (let i = 1; i < this.numberOfBreaks + 1; i++) {
+            breakAtTrailIndices.push(setSize * i);
+          }
+          if (breakAtTrailIndices.includes(this.currentTrial)) {
+            this.isBreak = true;
+          } else {
+            this.isBreak = false;
+            this.continueGame();
+          }
+        }
+      } else {
+        this.proceedtoNextStep();
+        await this.wait(2000);
+        this.proceedtoNextStep();
+        console.log(this.data);
+      }
+    }
+  }
+
+
+  /**
+   * Resume game
+   *
+   * @memberof GoNogoComponent
+   */
+  resume() {
+    this.reset();
+    this.isBreak = false;
+    this.continueGame();
+  }
+
+
+  /**
+   * Go ahead with next trial
+   *
+   * @memberof GoNogoComponent
+   */
+  async continueGame() {
+    await this.wait(this.interTrialDelay);
+    this.showStimulus();
+  }
+
+
+  /**
+   * Upload results
+   *
+   * @memberof GoNogoComponent
+   */
+  uploadResults() {
+  }
+
+
+  /**
+   * Go to dashboard or next experiment
+   *
+   * @memberof GoNogoComponent
+   */
+  continueAhead() {
+    this.router.navigate(['/dashboard']);
+  }
+
+
+
+  /**
+   * Reset trial data
+   *
+   * @memberof GoNogoComponent
+   */
+  reset() {
+    this.number = 0;
+    this.color = 'transparent';
+    this.feedback = '';
+    this.feedbackShown = false;
+    this.scoreForSpecificTrial = 0;
+  }
+
+
+  /**
+   * Reset all results
+   *
+   * @memberof GoNogoComponent
+   */
+  resetData() {
+    this.data = [];
+    this.totalScore = 0;
+  }
+
+
+  /**
+   * Start fullscreen
+   *
+   * @memberof GoNogoComponent
+   */
+  startGameInFullScreen() {
+    setFullScreen();
+  }
+
+
+  /**
+   * Delay function
+   *
+   * @param {number} time
+   * @returns {Promise<void>}
+   * @memberof GoNogoComponent
+   */
+  wait(time: number): Promise<void> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, time);
+    });
   }
 
 }
