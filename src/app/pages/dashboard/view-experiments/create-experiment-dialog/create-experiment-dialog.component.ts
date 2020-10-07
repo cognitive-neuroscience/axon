11517f7c73d7373c9550548ee89337064690ff8c
Nicholas Lee <niconal902@gmail.com>
2020-10-06 21:23:58 -0400

    setup create experiment dialog so that user can create multiple tasks with drag and drop functionality for reordering

--- src/app/pages/dashboard/view-experiments/create-experiment-dialog/create-experiment-dialog.component.ts
+++ src/app/pages/dashboard/view-experiments/create-experiment-dialog/create-experiment-dialog.component.ts
@@ -4,7 +4,19 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { TasklistService } from 'src/app/services/tasklist.service';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Experiment } from 'src/app/models/Experiment';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { SnackbarService } from '../../../../services/snackbar.service';
+
+export enum ArrayChange {
+  REMOVED,
+  ADDED
+}
+
+export class ArrayChangeObject {
+  change: ArrayChange;
+  item: string;
+}
 
 @Component({
   selector: 'app-create-experiment-dialog',
@@ -15,24 +27,76 @@ export class CreateExperimentDialogComponent implements OnInit, OnDestroy {
 
   tasks: Task[] = [];
   completedTasks: number[] = [];
+  selectedTasks: string[] = [];
 
   private subscriptions: Subscription[] = [];
 
   experimentForm = this.fb.group({
     name: ['', Validators.required],
     description: ['', Validators.maxLength(255)],
-    tasks: ['', Validators.required]
+    tasks: [[], Validators.required]
   })
 
   constructor(
     public dialogRef: MatDialogRef<CreateExperimentDialogComponent>,
     private tasklistService: TasklistService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private _snackbar: SnackbarService
   ) {}
 
   ngOnInit(): void {
     this.getTasklist()
     this.getCompletedTasklist()
+    this.subscriptions.push(
+      this.experimentForm.get("tasks").valueChanges.subscribe((tasks: string[]) => {
+        this.handleSelectChange(tasks)
+      })
+    )
+  }
+
+  handleSelectChange(newTasks: string[]) {
+    const change = this._findDifference(this.selectedTasks, newTasks)
+    switch (change.change) {
+      case ArrayChange.ADDED:
+        this.selectedTasks.push(change.item)
+        break;
+      case ArrayChange.REMOVED:
+        const indexOfRemovedItem = this.selectedTasks.indexOf(change.item)
+        this.selectedTasks.splice(indexOfRemovedItem, 1)
+        break;
+      default:
+        // we should never reach here
+        console.error("ArrayChange option not found")
+        this._snackbar.openErrorSnackbar("There was an error")
+        break;
+    }
+  }
+
+  // takes in 2 arrays and returns the single change between the two
+  private _findDifference(oldArr: string[], newArr: string[]): ArrayChangeObject {
+    // will either be 1 or -1 since only one change occurs at a time
+    const sizeDiff = newArr.length - oldArr.length
+    // element has been added to the array
+    if(sizeDiff > 0) {
+      // !<some-string> is false, !undefined is true so we search through
+      // until we come across a string in newArr that is not found in oldArr
+      const newStr = newArr.find(x => !oldArr.find(y => x === y))
+
+      return {
+        change: ArrayChange.ADDED,
+        item: newStr
+      }
+    } else {
+      // element has been removed from the array
+
+      // same logic as above, but reversed
+      const removedStr = oldArr.find(x => !newArr.find(y => x === y))
+
+      return {
+        change: ArrayChange.REMOVED,
+        item: removedStr
+      }
+    }
   }
 
   private getCompletedTasklist(): void {
@@ -41,6 +105,10 @@ export class CreateExperimentDialogComponent implements OnInit, OnDestroy {
         this.completedTasks = completedTasks
       })
     )
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.selectedTasks, event.previousIndex, event.currentIndex)
   }
 
   private getTasklist(): void {
@@ -53,16 +121,13 @@ export class CreateExperimentDialogComponent implements OnInit, OnDestroy {
 
   // TODO: implement multiple ordered task selection
   sendDataToParent() {
-    const assocTask = this.tasks.find(task => {
-      const selectedTaskName = this.experimentForm.get("tasks").value
-      return task.title === selectedTaskName;
-    })
+    const assocTasks = this.selectedTasks.map(selectedTask => this.tasks.find(task => selectedTask === task.title))
 
     const experiment = new Experiment(
       this.experimentForm.get("name").value,
-      null,
+      null, // code is populated in the backend
       this.experimentForm.get("description").value,
-      [assocTask]
+      assocTasks
     )
     this.dialogRef.close({experiment})
   }
