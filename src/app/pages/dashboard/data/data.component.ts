e2adcec8632895d09453ea6db95301abebfb9927
Nicholas Lee <niconal902@gmail.com>
2020-10-15 18:40:00 -0400

    added experiment_user download option

--- src/app/pages/dashboard/data/data.component.ts
+++ src/app/pages/dashboard/data/data.component.ts
@@ -28,19 +28,28 @@ export class DataComponent implements OnInit {
 
   downloadFile() {
     const splitSelectedTable = this.selectedTableName.split("_")
-    if(splitSelectedTable.length == 4) {
-      const experimentCode = splitSelectedTable[1]
-      const taskName = splitSelectedTable[3]
-      this._downloadDataService.getTableData(experimentCode, taskName).subscribe(data => {
-        if(!data) {
-          this.snackbarService.openErrorSnackbar("Could not get data")
-        } else {
-          this.excelService.exportAsExcel(data, this.selectedTableName)
-        }
-      }, err => {
-        console.error(err)
-        this.snackbarService.openErrorSnackbar("Could not get data")
-      })
+    // passed as first argument of url in GET request
+    let firstPart = ""
+    // passed as a second argument of url in GET request
+    let secondPart = ""
+    if(splitSelectedTable.length == 2) {
+      // in the case of tables like "experiment_user"
+      firstPart = splitSelectedTable[0]
+      secondPart = splitSelectedTable[1]
+    } else if(splitSelectedTable.length == 4) {
+      // for normal data tables of the form "experiment_<code>_task_<taskname>"
+      firstPart = splitSelectedTable[1]
+      secondPart =  splitSelectedTable[3]
     }
+    this._downloadDataService.getTableData(firstPart, secondPart).subscribe(data => {
+      if(!data) {
+        this.snackbarService.openErrorSnackbar("Could not get data")
+      } else {
+        this.excelService.exportAsExcel(data, this.selectedTableName)
+      }
+    }, err => {
+      console.error(err)
+      this.snackbarService.openErrorSnackbar("Could not get data")
+    })
   }
 }
