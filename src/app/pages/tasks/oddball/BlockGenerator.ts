b32bffff2ce8fe360b9248c362e7be3290647458
Nick Lee <nicholas.lee2@mail.mcgill.ca>
2021-01-21 22:08:04 -0500

    added pre loading for images to fix issue of show image delay

--- src/app/pages/tasks/oddball/BlockGenerator.ts
+++ src/app/pages/tasks/oddball/BlockGenerator.ts
@@ -1,13 +1,25 @@
+import { HttpClient } from "@angular/common/http";
+import { forkJoin, Observable, of } from "rxjs";
+import { catchError, map, take } from "rxjs/operators";
 export class OddballTrial {
     stimuli: string;
+    blob: Blob;
     isTarget: boolean;
+}
+
+export class ImageBlob {
+    [imgName: string]: Blob
 }
 
 export class BlockGenerator {
     
     trials: OddballTrial[];
 
-    private readonly scenesStimuli = [
+    // injected http client
+    private static http: HttpClient;
+
+    // hardcoded names of files
+    private static readonly imageNames: string[] = [
         "Scenes00010.png",
         "Scenes00015.png",
         "Scenes00047.png",
@@ -20,17 +32,60 @@ export class BlockGenerator {
         "Scenes00539.png",
         "Scenes00768.png",
         "Scenes00790.png",
+        "square.png",
+        "triangle.png",
     ]
+
+    // list of novel objects (not triangle or square)
+    private static novelBlobs: ImageBlob = {};
+    // list of stimuli objects (triangle and square)
+    private static stimuliBlobs: ImageBlob = {};
+
+    // called once on nginit to initialize the class and populate ImageBlobs
+    // we need to do this so that we don't get browser cache issues - images are preloaded
+    public static initialize(http: HttpClient): Observable<boolean> {
+        BlockGenerator.http = http;
+        const getArray = BlockGenerator.imageNames.map(img => BlockGenerator.http.get(`/assets/images/stimuli/oddball/${img}`, { responseType: 'blob' }))
+        // load up all of the get requests and sort them into the distinct ImageBlob objects, return true to oddball component when are done
+        return forkJoin(getArray).pipe(
+            take(1),
+            map(arr => {
+                arr.forEach((element: Blob, index) => {
+                    const name = BlockGenerator.imageNames[index];
+                    const imageBlobs = (name === 'square.png' || name === 'triangle.png' ? BlockGenerator.stimuliBlobs : BlockGenerator.novelBlobs);
+                    imageBlobs[name] = element;
+                })
+                return true;
+            }),
+            catchError(err => {
+                console.error(err)
+                return of(false)
+            })
+        )
+    }
+
+    get novelBlobSize(): number {
+        return Object.keys(BlockGenerator.novelBlobs).length;
+    }
+
+    get novelBlobKeys(): string[] {
+        return Object.keys(BlockGenerator.novelBlobs);
+    }
+
+    get stimuliBlobSize(): number {
+        return Object.keys(BlockGenerator).length;
+    }
 
     constructor(targetTrial: 'square.png' | 'triangle.png', numTargetTrials: number, numNovelTrials: number, numTotalTrials: number, scenesToExclude: string[] = []) {
         if((numTotalTrials - numTargetTrials - numNovelTrials) < 0) throw new Error("Number of total trials must be bigger or equal to the number of other types of trials");
-        if(numNovelTrials > (this.scenesStimuli.length - scenesToExclude.length)) throw new Error("Cannot create given number of novel stimuli without repeats")
+        if(numNovelTrials > (this.novelBlobSize - scenesToExclude.length)) throw new Error("Cannot create given number of novel stimuli without repeats")
 
         this.trials = new Array<OddballTrial>(numTotalTrials);
 
         // fill with non target trials
         this.trials.fill({
             stimuli: targetTrial === 'square.png' ? 'triange.png' : 'square.png',
+            blob: targetTrial === 'square.png' ? BlockGenerator.stimuliBlobs['triangle.png'] : BlockGenerator.stimuliBlobs['square.png'],
             isTarget: false
         })
 
@@ -38,31 +93,31 @@ export class BlockGenerator {
 
         // add the target trials
         for(let i = 0; i < numTargetTrials; i++) {
-            const randIndex = randIndices[randIndices.length - 1];
+            const randIndex = randIndices[i];
             this.trials[randIndex] = {
                 stimuli: targetTrial,
+                blob: BlockGenerator.stimuliBlobs[targetTrial],
                 isTarget: true
             }
-            randIndices.pop();
         }
 
         // add novel stimuli
-        for(let i = 0; i < numNovelTrials; i++) {
+        for(let i = numTargetTrials; i < numTargetTrials + numNovelTrials; i++) {
             const novelStimuli = this.getNovelStimuli(scenesToExclude);
-            const randIndex = randIndices[randIndices.length - 1];
+            const randIndex = randIndices[i];
             this.trials[randIndex] = {
                 stimuli: novelStimuli,
+                blob: BlockGenerator.novelBlobs[novelStimuli],
                 isTarget: false
             }
-            randIndices.pop();
         }
 
     }
 
     private getNovelStimuli(scenesToExclude: string[]): string {
-        let scene = this.scenesStimuli[this.getRandomNumber(0, this.scenesStimuli.length)];
+        let scene = this.novelBlobKeys[this.getRandomNumber(0, this.novelBlobSize)];
         while(scenesToExclude.includes(scene)) {
-            scene = this.scenesStimuli[this.getRandomNumber(0, this.scenesStimuli.length)];
+            scene = this.novelBlobKeys[this.getRandomNumber(0, this.novelBlobSize)];
         }
         scenesToExclude.push(scene)
         return scene;
