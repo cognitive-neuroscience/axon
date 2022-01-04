f18ca09b896dfdbeff04e0a92e69f23cc2ba2b44
Nicholas Lee <niconal902@gmail.com>
2022-01-03 22:04:59 -0800

    feat: updated consent reader to handle french translations

--- src/app/pages/shared/consent-component/consent-reader.component.ts
+++ src/app/pages/shared/consent-component/consent-reader.component.ts
@@ -1,28 +1,34 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { ITranslationText } from 'src/app/models/InternalDTOs';
 import { AbstractBaseReaderComponent } from '../../tasks/shared/base-reader';
 
+interface ConsentTextContent extends ITranslationText {
+    indent?: number;
+}
+
 class ConsentForm {
-    img: string;
-    title: string;
+    imgPath: string;
+    title: ITranslationText;
     summary: {
-        caption: string;
-        words: string[];
+        caption: ITranslationText;
+        words: ConsentTextContent[];
     }[];
-    secondTitle: string;
+    secondTitle: ITranslationText;
     body: {
-        caption: string;
-        words: string[];
+        caption: ITranslationText;
+        words: ConsentTextContent[];
     }[];
-    endMessage: string;
+    endMessage: ITranslationText;
     buttons: {
         reject: {
             show: boolean;
-            text: string;
+            text: ITranslationText;
         };
         accept: {
             show: boolean;
-            text: string;
+            text: ITranslationText;
         };
     };
 }
@@ -41,18 +47,52 @@ export class ConsentReaderComponent implements AbstractBaseReaderComponent {
     @Input()
     readerMetadata: ConsentNavigationConfig;
 
+    get imgPath(): string {
+        return this.readerMetadata.metadata?.imgPath || '';
+    }
+
+    get title(): string {
+        return this.getTextForLang(this.readerMetadata.metadata?.title) || '';
+    }
+
+    get secondTitle(): string {
+        return this.getTextForLang(this.readerMetadata.metadata?.secondTitle) || '';
+    }
+
+    get endMessage(): string {
+        return this.getTextForLang(this.readerMetadata.metadata?.endMessage) || '';
+    }
+
     @Output()
     emitConsent: EventEmitter<boolean> = new EventEmitter();
 
-    constructor(private router: Router) {
+    constructor(private router: Router, private translateService: TranslateService) {
         const state = this.router.getCurrentNavigation()?.extras.state as ConsentNavigationConfig;
 
-        if (state) {
-            this.readerMetadata = state;
-        }
+        if (state) this.readerMetadata = state;
     }
 
     onSubmit(response: boolean) {
         this.emitConsent.next(response);
+    }
+
+    getTextForLang(textObj: ITranslationText | string): string {
+        let lang = this.translateService.currentLang;
+        if (!lang) lang = 'en';
+
+        if (!textObj) {
+            // textObj is falsy
+            return '';
+        } else if (typeof textObj === 'string') {
+            // for backwards compatibility sake, textObj is just a plain string with no translation
+            return textObj;
+        } else if (!textObj[lang]) {
+            // no translation for the given language
+            const hasEnglish = !textObj['en'];
+            // also no translation for english
+            if (!hasEnglish) return '';
+        } else {
+            return textObj[lang];
+        }
     }
 }
