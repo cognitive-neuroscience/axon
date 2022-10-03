6452922e740b65416ba806e05527e087fe5c8d4b
Nick Lee <nicholas.lee2@mail.mcgill.ca>
2022-10-03 13:47:28 -0400

    feat: added csv support

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -10,25 +10,38 @@ import { SnackbarService } from './snackbar/snackbar.service';
 export class FileService {
     constructor(private snackbarService: SnackbarService) {}
 
-    private EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    // private EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
     private JSON_TYPE = 'text/plain;charset=UTF-8';
-    private EXCEL_EXTENSION = '.xlsx';
+    // private EXCEL_EXTENSION = '.xlsx';
     private JSON_EXTENSION = '.json';
+    private CSV_EXTENSION = '.csv';
 
-    exportAsExcel(tableData: DataTableFormat[], excelFileName: string): void {
-        const excelFormattedTableData = tableData.reduce((acc, curVal) => {
+    // exportAsExcel(tableData: DataTableFormat[], excelFileName: string): void {
+    //     const excelFormattedTableData = tableData.reduce((acc, curVal) => {
+    //         return acc.concat([{ ...curVal.fields }, ...curVal.expandable]);
+    //     }, []);
+
+    //     const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(excelFormattedTableData);
+
+    //     const workbook: XLSX.WorkBook = {
+    //         Sheets: { data: worksheet },
+    //         SheetNames: ['data'],
+    //     };
+
+    //     const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+
+    //     this._saveAsExcelFile(excelBuffer, excelFileName);
+    // }
+
+    exportAsCSV(tableData: DataTableFormat[], csvFileName: string): void {
+        const sheetFormattedTableData = tableData.reduce((acc, curVal) => {
             return acc.concat([{ ...curVal.fields }, ...curVal.expandable]);
         }, []);
-        const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(excelFormattedTableData);
 
-        const workbook: XLSX.WorkBook = {
-            Sheets: { data: worksheet },
-            SheetNames: ['data'],
-        };
-
-        const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
-
-        this._saveAsExcelFile(excelBuffer, excelFileName);
+        const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(sheetFormattedTableData);
+        const workbook: XLSX.WorkBook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet);
+        XLSX.writeFile(workbook, `${csvFileName}${this.CSV_EXTENSION}`, { bookType: 'csv' });
     }
 
     exportAsJSONFile(json: { [key: string]: any }, fileName: string) {
@@ -41,8 +54,8 @@ export class FileService {
         }
     }
 
-    private _saveAsExcelFile(buffer: any, fileName: string): void {
-        const data: Blob = new Blob([buffer], { type: this.EXCEL_TYPE });
-        FileSaver.saveAs(data, fileName + this.EXCEL_EXTENSION);
-    }
+    // private _saveAsExcelFile(buffer: any, fileName: string): void {
+    //     const data: Blob = new Blob([buffer], { type: this.EXCEL_TYPE });
+    //     FileSaver.saveAs(data, fileName + this.EXCEL_EXTENSION);
+    // }
 }
