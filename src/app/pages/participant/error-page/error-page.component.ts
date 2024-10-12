86fd507a217cf3885a7d4ddb5486248459cc1454
Nicholas Lee <niconal902@gmail.com>
2024-10-12 16:01:04 -0400

    feat: added redirect

--- src/app/pages/participant/error-page/error-page.component.ts
+++ src/app/pages/participant/error-page/error-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SessionStorageService } from '../../../services/sessionStorage.service';
 import { SnackbarService } from '../../../services/snackbar/snackbar.service';
+import { Role } from 'src/app/models/enums';
+import { UserStateService } from 'src/app/services/user-state-service';
 
 export interface IErrorNavigationState {
     studyId?: number;
@@ -19,14 +21,35 @@ export class ErrorPageComponent implements OnInit {
     constructor(
         private _snackbar: SnackbarService,
         private _router: Router,
-        private _sessionStorage: SessionStorageService
+        private _sessionStorage: SessionStorageService,
+        private _userStateService: UserStateService
     ) {
         const params = this._router.getCurrentNavigation()?.extras?.state as IErrorNavigationState;
-        this.copyableErrString = `User ID: ${params?.userId}\nStudy ID: ${params.studyId}\nTask Index: ${params.taskIndex}\n\n${params.stackTrace}`;
+        this.copyableErrString = `User ID: ${params?.userId}\nStudy ID: ${params?.studyId}\nTask Index: ${params?.taskIndex}\n\n${params?.stackTrace}`;
         this.copyableErrString = this.copyableErrString.trim();
     }
 
     copyableErrString: string = '';
 
     ngOnInit(): void {}
+
+    navigateToMain() {
+        if (this._userStateService.isCrowdsourcedUser) {
+            if (this._userStateService.currentlyRunningStudyId) {
+                this._router.navigateByUrl(
+                    `crowdsource-participant?studyid=${this._userStateService.currentlyRunningStudyId}`
+                );
+            } else {
+                this._router.navigate([`crowdsource-participant`]);
+            }
+        } else if (this._userStateService.userIsAdmin) {
+            this._router.navigate([`admin-dashboard`]);
+        } else if (this._userStateService.userIsParticipant) {
+            this._router.navigate([`participant-dashboard`]);
+        } else if (this._userStateService.userIsGuest || this._userStateService.userIsOrgMember) {
+            this._router.navigate([`organization-member-dashboard`]);
+        } else {
+            throw new Error('Unsupported user role!');
+        }
+    }
 }
